3cbb10fa19b78cd1e1d6fd9d60fb0d98778b7670
Shota Fukumori (sora_h) <sorah@tubusu.net>
2011-08-20 11:42:29 +0900

    hi

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Iamcamper::Application.config.secret_token = '5b8b8f3e3f8bb69f0290f8a4be6ed9ba60726c2fd9c0f6a610749e807a8d017f85ebc7fae8749cb9814b37686c00d28734ad2f5dffe1accbbf34ae06acec9353'
