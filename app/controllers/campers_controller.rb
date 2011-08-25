f56dfc0ebbb5d3e03777d39eb303c68423ddc27b
Kazunori Jo (itochan) <itochan315@gmail.com>
2011-08-25 18:29:43 +0900

    fixed bug

--- app/controllers/campers_controller.rb
+++ app/controllers/campers_controller.rb
@@ -25,6 +25,7 @@ class CampersController < ApplicationController
   # GET /campers/new.json
   def new
     @camper = Camper.new
+    @courses = Course.all.map{ |c| [c.name, c.id] }
 
     respond_to do |format|
       format.html # new.html.erb
@@ -35,6 +36,7 @@ class CampersController < ApplicationController
   # GET /campers/1/edit
   def edit
     @camper = Camper.find(params[:id])
+    @courses = Course.all.map{ |c| [c.name, c.id] }
   end
 
   # POST /campers
